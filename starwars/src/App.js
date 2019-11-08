d62fa3c4521e85eb4103615af10071d66bf0a27a
lukasjaronis <jaronisluke13@gmail.com>
2019-11-08 11:02:06 -0500

    init

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
