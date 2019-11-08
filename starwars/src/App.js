f4cecea0af058642c6bb576e61c59f7e07a41484
lukasjaronis <jaronisluke13@gmail.com>
2019-11-08 11:25:08 -0500

    styling done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import StarCard from "./components/StarCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppStyles } from "./components/styles";
+import styled from "styled-components";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,10 +12,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+  const App_container = styled.div`
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem auto;
+  `;
+
+
+
   return (
+    <App_container>
+    <h1 className="Header">React Wars</h1>
     <AppStyles>
-      <h1 className="Header">React Wars</h1>
+      <StarCard />
       </AppStyles>
+      </App_container>
   );
 }
 
--- starwars/src/components/styles.js
+++ starwars/src/components/styles.js
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
 export const AppStyles = styled.div`
-display: grid;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
 background: rgba(245,245,245,0.5);
 margin: 3rem auto;
-height: 90vh;
+height: 100%;
 width: 90%;
 border-radius: 15px;
 
 .Header {
+     display: flex;
+     align-self: center;
      text-align: center;
      letter-spacing: 1px;
      background: red;
@@ -21,10 +25,11 @@ flex-direction: column;
 justify-content: flex-start;
 align-items: center;
 width: 400px;
-height: 400px;
+height: 500px;
 background: rgba(245,245,245,0.6);
 border: 1px solid black;
 border-radius: 15px;
+margin: 2rem;
 
 h2 {
      margin-top: 50px;
